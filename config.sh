d2308ff43ba9761ee33090991470f3d590486da7
riag <riag@163.com>
2018-10-31 16:06:42 +0800

    增加几个有用的包

--- config.sh
+++ config.sh
@@ -5,3 +5,7 @@ EXTRA_PACKAGES+=(git)
 EXTRA_PACKAGES+=(util-linux) # temp, should be added to manjaro-tools
 EXTRA_PACKAGES+=(haveged) # for pacman-init
 EXTRA_PACKAGES+=(manjaro-tools-pkg sudo gzip)
+# utils tools, like ps netstate command tools
+EXTRA_PACKAGES+=(net-tools dnsutils inetutils iproute2 iputils procps)
+EXTRA_PACKAGES+=(neovim tmux)
+
