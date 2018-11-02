fac8c7798aa78a753be88752f526017bbd90d341
riag <riag@163.com>
2018-11-02 11:29:08 +0800

    精简要安装的包

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # config.sh: to override built in defaults
 
-EXTRA_PACKAGES+=(git)
 EXTRA_PACKAGES+=(util-linux) # temp, should be added to manjaro-tools
 EXTRA_PACKAGES+=(haveged) # for pacman-init
 EXTRA_PACKAGES+=(manjaro-tools-pkg sudo gzip)
 # utils tools, like ps netstate command tools
 EXTRA_PACKAGES+=(net-tools dnsutils inetutils iproute2 iputils procps)
-EXTRA_PACKAGES+=(neovim tmux)
+#EXTRA_PACKAGES+=(git)
+#EXTRA_PACKAGES+=(neovim tmux)
 
